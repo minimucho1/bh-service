6a0156df4def39dbbde16f6eb92eec310fb29fb6
jknguyen22 <jen.nguyen916@gmail.com>
2018-12-15 20:12:16 -0600

    Issue #1 | Jenny | Added /anime route

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var animeRouter = require('./routes/anime');
 
 var app = express();
 
@@ -14,13 +15,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json()); // respond with json
+app.use(express.urlencoded({ extended: false })); // has to do with body parser
+app.use(cookieParser()); // reads cookies for us
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// define endpoint '/anime'
+app.use('/anime', animeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,3 +42,20 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+// var express = require("express");
+// var bodyParser = require("body-parser");
+// var routes = require("./routes/routes.js");
+// var app = express();
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// routes(app);
+
+// var server = app.listen(3001, function () {
+//     console.log("app running on port.", server.address().port);
+// });
+
+
+// module.exports = app;
